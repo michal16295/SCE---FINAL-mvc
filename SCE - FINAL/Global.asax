b4f0afe03b0b7850aad7eb29a47b170fc87e2707
michalbarski <michalbarski@MICHALBARSK32D9>
2020-01-02 13:05:28 +0200

    Add project files.

---
+++ SCE - FINAL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCE___FINAL.MvcApplication" Language="C#" %>
---
+++ SCE - FINAL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SCE___FINAL.Startup))]
+namespace SCE___FINAL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
